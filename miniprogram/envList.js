85925b94942391ee1bf17357a8e02263d62ce7ae
crq <2631315464@qq.com>
2021-11-28 01:15:05 +0800

    初始化云音乐小程序

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-4g39q6fn18a20613","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"D:\Program\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-4g39q6fn18a20613 --n quickstartFunctions --r --project "D:\dev\wechat\cloud-music" --report_start --report
